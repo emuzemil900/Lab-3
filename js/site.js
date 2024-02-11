dc7e3a029e10dfcbdfdaeb1e32479e49e9c83b3a
emuzemil900 <143541381+emuzemil900@users.noreply.github.com>
2024-02-10 18:49:33 -0600

    Update site.js

--- js/site.js
+++ js/site.js
@@ -3,3 +3,8 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 });
+
+$('#alt-box').on('click',function() {
+  $('#age-verification').prop('checked',true);
+  $('#alt-box').html('<p>&#10004;</p>');
+});
